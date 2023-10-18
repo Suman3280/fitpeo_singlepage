e09e3b5cdf531ea9e8452d7bb19c99891707bfe6
Suman3280 <sumansingh2152002@gmail.com>
2023-10-18 15:57:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <div className='flex flex-col lg:flex-row'>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
